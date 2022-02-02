d007969e0784a32e7f31b58b51ce027ae6058706
fernandojunior210696 <38145452+fernandojunior210696@users.noreply.github.com>
2022-02-01 23:38:52 -0300

    Update streamlit_sharing.py

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -7,14 +7,16 @@ from IPython.core.display import HTML
 import plotly.express as px
 import plotly.graph_objects as go
 
-# Título do dashboard
-# st.title("Catálogo Woods Wine")
-st.markdown("<h1 style='text-align: center; '>Catálogo Woods Wine</h1>", unsafe_allow_html=True)
+logo, title = st.columns(2)
 
-st.sidebar.image('./images/logo_woods.png',use_column_width='always')
+with logo:
+    st.image('./images/logo_woods.png',use_column_width='auto')
 
-st.sidebar.write(" ")
-st.sidebar.markdown("## Filtre abaixo por país, tipo de uva, faixa de preço ou pelo nome do vinho e faça sua escolha!")
+with title:
+    st.markdown("<h2 style='text-align: right; line-height: 100px; '>Catálogo Woods Wine</h2>", unsafe_allow_html=True)
+
+st.markdown("---")
+st.markdown("<p style='text-align: center; font-size: 32px; '>Filtre abaixo por país, tipo de uva, faixa de preço ou pelo nome do vinho e faça sua escolha!</p>", unsafe_allow_html=True)
 
 @st.cache
 def get_data(url):
@@ -22,7 +24,7 @@ def get_data(url):
     gsheet_url = url
     # gsheet_url = st.secrets["public_gsheets_url"]
     conn = connect()
-    rows = conn.execute(f'SELECT NOME AS wine_name, TIPO AS grape_type, ORIGEM AS country, VALOR AS price, BANDEIRA AS flag  FROM "{gsheet_url}"')
+    rows = conn.execute(f'SELECT NOME AS wine_name, TIPO AS grape_type, ORIGEM AS country, VALOR AS price FROM "{gsheet_url}"')
 
     # Converte dados para pandas df
     df_gsheet = pd.DataFrame(rows)
@@ -40,23 +42,24 @@ def path_to_image_html(path):
 
 df_gsheet = get_data(st.secrets["public_gsheets_url"])
 
-values = st.sidebar.slider(label = "Preço dos vinhos", 
+values = st.slider(label = "Preço dos vinhos", 
                            min_value = df_gsheet.price.min(), 
                            max_value = df_gsheet.price.max(),
-                           value = (float(df_gsheet.price.min()), float(df_gsheet.price.max())))
+                           value = (float(df_gsheet.price.min()), float(df_gsheet.price.max())),
+                           format = "R$%g")
 
 # Selected price range
 min_price=values[0]
 max_price=values[1]
 
 # Selected country
-country = st.sidebar.selectbox("Selecione o país", ['selecione'] + list(df_gsheet.country.unique()), 0)
+country = st.selectbox("Selecione o país", ['selecione'] + list(df_gsheet.country.unique()), 0)
 
 # Selected type
-grape_type = st.sidebar.selectbox("Selecione o tipo de uva", ['selecione'] + list(df_gsheet.grape_type.unique()), 0)
+grape_type = st.selectbox("Selecione o tipo de uva", ['selecione'] + list(df_gsheet.grape_type.unique()), 0)
 
 # Selected wine
-wine_name = st.sidebar.selectbox("Selecione o nome do vinho", ['selecione'] + list(df_gsheet.wine_name.unique()), 0)
+wine_name = st.selectbox("Selecione o nome do vinho", ['selecione'] + list(df_gsheet.wine_name.unique()), 0)
 
 df_filtered = df_gsheet[(df_gsheet.price>=min_price) & (df_gsheet.price<=max_price)]
 if country != 'selecione':
@@ -66,14 +69,46 @@ if wine_name != 'selecione':
 if grape_type != 'selecione':
     df_filtered = df_filtered[df_filtered.grape_type == grape_type]
 
-fig = go.Figure(data = go.Table(columnwidth=[], 
-header=dict(values=["Vinho", "Tipo", "País", "Valor (R$)"], fill_color='#d73844', align='center'),
-cells = dict(values=[df_filtered.wine_name, df_filtered.grape_type, df_filtered.country, df_filtered.price], align='left')
-))
+# fig = go.Figure(data = go.Table(columnwidth=[], 
+# header=dict(values=["Vinho", "Tipo", "País", "Valor (R$)"], fill_color='#d73844', align='center'),
+# cells = dict(values=[df_filtered.wine_name, df_filtered.grape_type, df_filtered.country, df_filtered.price], align='left')
+# ))
 
-fig.update_layout(margin=dict(l=5, r=5, b=10, t=10),font=dict(size=18))
+# fig.update_layout(margin=dict(l=5, r=5, b=10, t=10),font=dict(size=18))
 
-config = {'displayModeBar': False}
-st.plotly_chart(fig, config=config)
+# config = {'displayModeBar': False}
+# st.plotly_chart(fig, config=config)
 
-# st.write(HTML(df_filtered['flag'].to_html(escape=False ,formatters=dict(flag=path_to_image_html))))
+df_filtered['price'] = df_filtered['price'].apply(lambda x: "R${:.2f}".format(x))
+df_filtered.rename(columns={"wine_name": "Nome", "grape_type": "Tipo", "country": "País", "price": "Valor"}, inplace=True)
+
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+# Display a static table
+# st.table(df_filtered)
+
+headers = {
+    'selector': 'th:not(.index_name)',
+    'props': [('background-color', '#d73844'), ('color', 'white')]
+}
+
+rows = {
+    'selector': 'td',
+    'props': [('background-color', '#F0F2F6'), ('color', '#000000')]
+}
+
+st.table(df_filtered.style.hide_index().set_table_styles([headers, rows]))
+
+
+# st.table(df_filtered.style.hide_index().set_table_styles(
+#    [{'selector': 'th',
+#    'props': [('background-color', '#d73844'),('font color', '#000000')]}]))
