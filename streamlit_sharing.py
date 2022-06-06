8830362be263b2aa8b7ea831278a511ddcb7badd
fernandojunior210696 <38145452+fernandojunior210696@users.noreply.github.com>
2022-06-06 12:13:17 -0300

    fix/ change all numericals to float

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -98,22 +98,22 @@ with my_expander:
     st.markdown(price_settings, unsafe_allow_html=True)
     
     # adjust inicial labels
-    min_price_inicial_value = max(10.0, df_gsheet.price.min())
-    max_price_inicial_value = min(24000.0, df_gsheet.price.max())
+    min_price_inicial_value = float(max(10.0, df_gsheet.price.min()))
+    max_price_inicial_value = float(min(24000.0, df_gsheet.price.max()))
     
     col1, col2 = st.columns(2)
     with col1:
         min_price = st.number_input(label = 'A partir de R$',
-                           min_value = df_gsheet.price.min(), 
-                           max_value = df_gsheet.price.max(),
+                           min_value = float(df_gsheet.price.min()), 
+                           max_value = float(df_gsheet.price.max()),
                            format = "%g",
                            value = min_price_inicial_value,
                            step = 10.0)
 
     with col2:
         max_price = st.number_input(label = 'Até R$',
-                           min_value = df_gsheet.price.min(), 
-                           max_value = df_gsheet.price.max(),
+                           min_value = float(df_gsheet.price.min()), 
+                           max_value = float(df_gsheet.price.max()),
                            value = max_price_inicial_value,
                            format = "%g",
                            step = 10.0)
